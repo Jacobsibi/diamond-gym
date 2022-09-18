7efcf6ca4c0a4f17cee81533fadd982e157b057b
Jacob <jacobssibi@gmail.com>
2022-09-19 05:17:21 +1200

    re-commit

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -7,11 +7,11 @@ const Navbar = () => {
     return (
         <Stack 
             direction="row"
-            justifyfyContent="space-around"
-            sx={{gap: { sm: '122px', xs: '40px'}, mt: {
-                sm:'32px', xs: '20px'}, justifyContent: 'none'
-            }} px="20px"
-        >
+            justifyContent="space-around"
+            sx={{ gap: { sm: '123px', xs: '40px'}, 
+                  mt:  { sm:'32px', xs: '20px'}, 
+                  justifyContent: 'none'
+            }} px="20px">
             <Link to="/">
                 <img src={Logo} alt="logo" style={{width: '48px', height: '48px', margin: '0 20px'}} />
             </Link>
